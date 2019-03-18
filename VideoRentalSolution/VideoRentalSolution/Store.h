619d3ccb9a75d8bc85f56792871dd5f801671211
Kier <kierf@uw.edu>
2019-03-18 15:06:39 -0700

    Initialize inventory(videos) and customers done. TODO: parse and execute commands

---
+++ VideoRentalSolution/VideoRentalSolution/Store.h
@@ -0,0 +1,29 @@
+#ifndef STORE_H
+#define STORE_H
+#include <string>
+#include <vector>
+#include "Video.h"
+#include "Customer.h"
+#include "hashtable.h"
+
+
+using namespace std;
+
+class Store
+{
+public:
+	Store();
+	~Store();
+	void initializeInventory(ifstream);
+	void initializeCustomers(ifstream);
+	void processCommands(ifstream);
+
+private:
+	vector<Video*> videoList;
+	HashTable customerList;
+
+	bool containsVideo(Video*);
+	bool updateStock(string, int);
+};
+
+#endif
