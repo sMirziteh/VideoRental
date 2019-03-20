3fb1fd6b0e62586c5077834305f185b0149f9eac
shelby mirziteh <shelby.mirziteh@gmail.com>
2019-03-19 22:37:57 -0700

    accepted jordan's changes

--- hashtable.h
+++ hashtable.h
@@ -12,32 +12,21 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 #include "Customer.h"
-<<<<<<< HEAD
-=======
 #include <vector>
->>>>>>> Jordan
 
 class HashTable
 {
-public:
+  public:
 	HashTable();
 	~HashTable();
 	void add(Customer *);
 	Customer *get(int);
-<<<<<<< HEAD
-	void remove(Customer *);
-=======
 	void remove(int);
->>>>>>> Jordan
 	int hash(int);
 	bool isFull();
 
-private:
-<<<<<<< HEAD
-	vector<Customer> objectList(10) = {nullptr};
-=======
+  private:
 	vector<Customer *> customerList;
->>>>>>> Jordan
 };
 
 #endif // HASHTABLE_H
