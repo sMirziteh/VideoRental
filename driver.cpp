eeb84e957767b6caf591b9bc276096d9e26972c3
shelby mirziteh <shelby.mirziteh@gmail.com>
2019-03-20 21:41:57 -0700

    added print statements to driver

--- driver.cpp
+++ driver.cpp
@@ -8,33 +8,43 @@ using namespace std;
 
 int main()
 {
-	Store Blockbuster;
-	ifstream movies("data4movies.txt");
-	if (!movies)
-	{
-		cout << "data4movies.txt could not be opened." << endl;
-		return 1;
-	}
-	else cout << "Opening data4movies.txt" << endl;
-	Blockbuster.initializeInventory(movies);
+    cout << "Welcome to Blockbuster inventory system!" << endl
+         << endl;
+    Store Blockbuster;
+    ifstream movies("data4movies.txt");
+    if (!movies)
+    {
+        cout << "data4movies.txt could not be opened." << endl
+             << endl;
+        return 1;
+    }
+    else
+        cout << "Opening data4movies.txt" << endl;
+    Blockbuster.initializeInventory(movies);
 
-	ifstream customers("data4customers.txt");
-	if (!customers)
-	{
-		cout << "data4customers.txt could not be opened." << endl;
-		return 1;
-	}
-	else cout << "Opening data4customers.txt" << endl;
-	Blockbuster.initializeCustomers(customers);
+    ifstream customers("data4customers.txt");
+    if (!customers)
+    {
+        cout << "data4customers.txt could not be opened." << endl
+             << endl;
+        return 1;
+    }
+    else
+        cout << "Opening data4customers.txt" << endl
+             << endl;
+    Blockbuster.initializeCustomers(customers);
 
-	ifstream commands("data4commands.txt");
-	if (!commands)
-	{
-		cout << "data4commands.txt could not be opened." << endl;
-		return 1;
-	}
-	else cout << "Opening data4commands.txt" << endl;
-	Blockbuster.processCommands(commands);
+    ifstream commands("data4commands.txt");
+    if (!commands)
+    {
+        cout << "data4commands.txt could not be opened." << endl
+             << endl;
+        return 1;
+    }
+    else
+        cout << "Opening data4commands.txt" << endl
+             << endl;
+    Blockbuster.processCommands(commands);
 
-	return 0;
+    return 0;
 }
